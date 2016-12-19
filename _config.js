44e4cccf337f3f022b5ba0f10934c9d598b9ee18
Jack Ratner <jackratner@gmail.com>
2016-12-18 20:14:03 -0500

    it works

--- _config.js
+++ _config.js
@@ -2,6 +2,7 @@ module.exports = {
   userID: '<lastfm user id>',
   lastfmApiKey: '<lastfm api key>',
   currentTrackCheckInterval: 10000, // ms interval to check with lastfm api for song change
+  writeInterval: 1,
   outputResolution: {
     width: 32,
     height: 32
