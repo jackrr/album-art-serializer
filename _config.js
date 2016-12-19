765abf7e57f0069e0583d1b09d04554858a8ace7
Jack Ratner <jackratner@gmail.com>
2016-12-18 19:40:39 -0500

    image pulling, pixeling, and serialization

---
+++ _config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  userID: '<lastfm user id>',
+  lastfmApiKey: '<lastfm api key>',
+  currentTrackCheckInterval: 10000, // ms interval to check with lastfm api for song change
+  outputResolution: {
+    width: 32,
+    height: 32
+  }
+}
---
+++ run.js
@@ -0,0 +1,34 @@
+let lastfm = require('./lib/lastfm');
+let image = require('./lib/image');
+let config = require('./config');
+let serial = require('./lib/serial');
+
+var userID = config.userID
+var artworkFolder = config.artworkPath
+
+
+var nextSong = function(err, song){
+  if (err) {
+    return console.log("error getting current song:", err)
+  }
+
+  console.log('Track:', song.name, '\nAlbum: ', song.album, '\nArtist:', song.artist);
+
+  if (!song.albumUrl || !song.albumUrl.length) {
+    return console.log("no albumUrl given for song", song.name)
+  }
+
+  var url = song.albumUrl
+  var fname = Date.now().toString()+'.'+url[url.length-3] + url[url.length-2] + url[url.length-1]
+
+  var albumPath = artworkFolder + fname
+  image.getPixelGrid(song.albumUrl, function(err, grid){
+    if (err) {
+      console.log(err)
+    } else {
+      serial.sendImage(grid);
+    }
+  }, {tile: true})
+}
+
+lastfm.onSongChange(userID, nextSong)
