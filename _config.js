40d28d679b3205d2420cc47b6503bbdf03ed2783
Jack Ratner <jackratner@gmail.com>
2016-12-27 21:37:26 -0500

    up baud rate, modify timeouts

--- _config.js
+++ _config.js
@@ -2,7 +2,6 @@ module.exports = {
   userID: '<lastfm user id>',
   lastfmApiKey: '<lastfm api key>',
   currentTrackCheckInterval: 10000, // ms interval to check with lastfm api for song change
-  writeInterval: 20,
   outputResolution: {
     width: 32,
     height: 32
